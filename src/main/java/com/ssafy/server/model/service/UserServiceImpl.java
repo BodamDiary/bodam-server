e40e5f5abc79b7a0238d8dbbc0af485206714231
Dahee Park <ekgml3219@gmail.com>
2024-11-08 20:59:29 +0900

    :sparkles: feat: add UserServiceImpl

--- src/main/java/com/ssafy/server/model/service/UserServiceImpl.java
+++ src/main/java/com/ssafy/server/model/service/UserServiceImpl.java
@@ -1,7 +1,9 @@
 package com.ssafy.server.model.service;
 
 import com.ssafy.server.model.dao.UserMapper;
+import com.ssafy.server.model.daosc.SaltMapper;
 import com.ssafy.server.model.dto.User;
+import com.ssafy.server.util.OpenCrypt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,14 +15,26 @@ public class UserServiceImpl implements UserService{
     @Autowired
     private UserMapper userMapper;
 
+    @Autowired
+    private SaltMapper saltMapper;
+
     @Override
     public int registUser(User user) {
+        String salt = OpenCrypt.encryptPw(user);
+
+        saltMapper.insertSecuInfo(user.getUserId(), salt);
+
         return userMapper.insertUser(user);
     }
 
     @Override
     public User getUser(int userId) {
         return userMapper.selectUser(userId);
+    }
+
+    @Override
+    public User getUserByEmail(String email) {
+        return userMapper.selectUserByEmail(email);
     }
 
     @Override
@@ -38,6 +52,21 @@ public class UserServiceImpl implements UserService{
     public int removeUser(int userId) {
         int isRemoveUser = userMapper.deleteUser(userId);
         return isRemoveUser;
+    }
+
+    @Override
+    public User loginUser(String email, String password) {
+        User user = userMapper.selectUserByEmail(email);
+        String salt = saltMapper.selectSalt(user.getUserId());
+
+        String hashedPassword = OpenCrypt.byteArrayToHex(OpenCrypt.getSHA256(password, salt));
+
+        if (user.getPassword().equals(hashedPassword)) {
+            user.setPassword("");
+            return user;
+        }
+
+        return null;
     }
 
 }
