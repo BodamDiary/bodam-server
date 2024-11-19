fd0ae2f016b7437524c925aeeafeffff7e0e5bdb
Dahee Park <ekgml3219@gmail.com>
2024-11-20 08:37:06 +0900

    :sparkles: feat: add HTTPS redirect configuration

--- src/main/java/com/ssafy/server/config/ServerConfig.java
+++ src/main/java/com/ssafy/server/config/ServerConfig.java
@@ -4,15 +4,23 @@ import org.apache.catalina.Context;
 import org.apache.catalina.connector.Connector;
 import org.apache.tomcat.util.descriptor.web.SecurityCollection;
 import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ServerConfig {
 
+    @Value("${server.http.port:80}")
+    private int httpPort;
+
+    @Value("${server.port:443}")
+    private int httpsPort;
+
     @Bean
-    public TomcatServletWebServerFactory servletContainer() {
+    public ServletWebServerFactory servletContainer() {
         TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
             @Override
             protected void postProcessContext(Context context) {
@@ -31,9 +39,9 @@ public class ServerConfig {
     private Connector redirectConnector() {
         Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
         connector.setScheme("http");
-        connector.setPort(80);
+        connector.setPort(httpPort);
         connector.setSecure(false);
-        connector.setRedirectPort(443);
+        connector.setRedirectPort(httpsPort);
         return connector;
     }
 }
