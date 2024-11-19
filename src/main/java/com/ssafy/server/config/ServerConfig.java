61484e3c6c6cb2b5f1b036f19a1c05c9de74f0e0
Dahee Park <ekgml3219@gmail.com>
2024-11-19 23:51:39 +0900

    :sparkles: feat: add HTTPS redirect configuration

--- src/main/java/com/ssafy/server/config/ServerConfig.java
+++ src/main/java/com/ssafy/server/config/ServerConfig.java
@@ -33,7 +33,7 @@ public class ServerConfig {
         Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
         connector.setScheme("http");
         connector.setPort(80);
-        connector.setSecure(false);
+        connector.setSecure(true);
         connector.setRedirectPort(443);
         return connector;
     }
