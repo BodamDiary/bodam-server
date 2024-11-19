3ad900fcfd0bd2e80c34a94aa059ad132d65e680
Dahee Park <ekgml3219@gmail.com>
2024-11-20 08:32:27 +0900

    :sparkles: feat: add HTTPS redirect configuration

--- src/main/java/com/ssafy/server/config/ServerConfig.java
+++ src/main/java/com/ssafy/server/config/ServerConfig.java
@@ -5,7 +5,6 @@ import org.apache.catalina.connector.Connector;
 import org.apache.tomcat.util.descriptor.web.SecurityCollection;
 import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,9 +12,8 @@ import org.springframework.context.annotation.Configuration;
 public class ServerConfig {
 
     @Bean
-    public ServletWebServerFactory servletContainer() {
-
-        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory(){
+    public TomcatServletWebServerFactory servletContainer() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
             @Override
             protected void postProcessContext(Context context) {
                 SecurityConstraint securityConstraint = new SecurityConstraint();
@@ -26,15 +24,15 @@ public class ServerConfig {
                 context.addConstraint(securityConstraint);
             }
         };
-        tomcat.addAdditionalTomcatConnectors(createSslConnector());
+        tomcat.addAdditionalTomcatConnectors(redirectConnector());
         return tomcat;
     }
 
-    private Connector createSslConnector() {
-        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+    private Connector redirectConnector() {
+        Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
         connector.setScheme("http");
-        connector.setSecure(false);
         connector.setPort(80);
+        connector.setSecure(false);
         connector.setRedirectPort(443);
         return connector;
     }
