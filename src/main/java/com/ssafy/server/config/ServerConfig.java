bbba459b2ef74c5992bf352354275fd54d37c44a
Dahee Park <ekgml3219@gmail.com>
2024-11-20 09:38:53 +0900

    :sparkles: feat: add HTTPS redirect configuration

--- src/main/java/com/ssafy/server/config/ServerConfig.java
+++ src/main/java/com/ssafy/server/config/ServerConfig.java
@@ -1,5 +1,7 @@
 package com.ssafy.server.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.apache.catalina.Context;
 import org.apache.catalina.connector.Connector;
 import org.apache.tomcat.util.descriptor.web.SecurityCollection;
@@ -13,6 +15,8 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class ServerConfig {
 
+    private static final Logger logger = LoggerFactory.getLogger(ServerConfig.class);
+
     @Value("${server.http.port:80}")
     private int httpPort;
 
@@ -21,9 +25,12 @@ public class ServerConfig {
 
     @Bean
     public ServletWebServerFactory servletContainer() {
+        logger.info("Configuring server with HTTP port: {} and HTTPS port: {}", httpPort, httpsPort);
+
         TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
             @Override
             protected void postProcessContext(Context context) {
+                logger.info("Configuring security constraints for HTTPS redirect");
                 SecurityConstraint securityConstraint = new SecurityConstraint();
                 securityConstraint.setUserConstraint("CONFIDENTIAL");
                 SecurityCollection collection = new SecurityCollection();
@@ -32,7 +39,10 @@ public class ServerConfig {
                 context.addConstraint(securityConstraint);
             }
         };
-        tomcat.addAdditionalTomcatConnectors(redirectConnector());
+
+        Connector connector = redirectConnector();
+        logger.info("Adding HTTP connector on port: {}", connector.getPort());
+        tomcat.addAdditionalTomcatConnectors(connector);
         return tomcat;
     }
 
@@ -42,6 +52,7 @@ public class ServerConfig {
         connector.setPort(httpPort);
         connector.setSecure(false);
         connector.setRedirectPort(httpsPort);
+
         return connector;
     }
 }
