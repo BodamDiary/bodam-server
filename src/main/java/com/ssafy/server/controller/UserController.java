a90326a4af3b2e445582f8f782a6849727872194
Daeun <syoon4486@gmail.com>
2024-11-20 10:35:05 +0900

    :bug: fix user regist

--- src/main/java/com/ssafy/server/controller/UserController.java
+++ src/main/java/com/ssafy/server/controller/UserController.java
@@ -4,22 +4,15 @@ import com.ssafy.server.model.dto.User;
 import com.ssafy.server.model.service.UserService;
 import com.ssafy.server.util.JwtTokenProvider;
 import jakarta.servlet.http.Cookie;
-import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-
-import java.io.IOException;
-import java.time.LocalDateTime;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @RequiredArgsConstructor
 @RestController
