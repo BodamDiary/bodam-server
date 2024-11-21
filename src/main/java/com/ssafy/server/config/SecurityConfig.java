914cd3c24d0f9303a39dbfa56922278244cf0ceb
Dahee Park <ekgml3219@gmail.com>
2024-11-21 13:38:25 +0900

    :sparkles: feat: add ssl security and exclude login page

---
+++ src/main/java/com/ssafy/server/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.ssafy.server.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@Profile("prod")
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .requiresChannel()
+                .anyRequest()
+                .requiresSecure(); // 모든 요청을 HTTPS로 리다이렉트
+
+        return http.build();
+    }
+}
