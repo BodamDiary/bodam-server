76374e435ddfe6b6ed30fd1a25834b7bec4620dc
Dahee Park <ekgml3219@gmail.com>
2024-11-25 11:59:25 +0900

    :bug: add logs

--- src/main/java/com/ssafy/server/config/SecurityConfig.java
+++ src/main/java/com/ssafy/server/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -44,6 +45,7 @@ public class SecurityConfig {
 
                 // 요청 URL별 인증 설정
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.HEAD, "/**").permitAll()
                         .requestMatchers("/content/**", "/users/login-user", "/users/regist-user", "/kakao", "/regist-kakao", "/kakao-login", "/swagger-ui/").permitAll()  // 인증 없이 접근 가능한 경로
                         .anyRequest().authenticated()  // 그 외 요청은 인증 필요
                 )
