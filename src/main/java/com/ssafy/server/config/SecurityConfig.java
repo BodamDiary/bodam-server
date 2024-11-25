124e14884708030954f593be5825790d9506a991
Dahee Park <ekgml3219@gmail.com>
2024-11-25 11:30:18 +0900

    :bug: add logs

--- src/main/java/com/ssafy/server/config/SecurityConfig.java
+++ src/main/java/com/ssafy/server/config/SecurityConfig.java
@@ -44,7 +44,7 @@ public class SecurityConfig {
 
                 // 요청 URL별 인증 설정
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/content/**", "/users/login-user", "/users/regist-user", "/kakao", "/regist-kakao", "kakao-login").permitAll()  // 인증 없이 접근 가능한 경로
+                        .requestMatchers("/content/**", "/users/login-user", "/users/regist-user", "/kakao", "/regist-kakao", "/kakao-login", "/swagger-ui/").permitAll()  // 인증 없이 접근 가능한 경로
                         .anyRequest().authenticated()  // 그 외 요청은 인증 필요
                 )
 
